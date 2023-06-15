bbb8d8b49bad3fef699c0d6c609926617fe3e9b4
Marcelo Santesso Semeghini <marcelosemeghini@gmail.com>
2023-06-15 17:24:04 -0300

    last try

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/production-sub-path/'
-    : '/pokedex'
+  publicPath: '/'
+  //process.env.NODE_ENV === 'production'
+  //  ? '/pokedex/'
+  //  : '/'
 }
